70425c1d16127c31031f137fbc2717f91c077162
Elsacoutelier <47112756+Elsacoutelier@users.noreply.github.com>
2019-01-29 10:43:22 +0100

    ajout de commentaires 1

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,22 @@
 package com.example;
 
-
+/**
+* This is a class
+*/
 public class Greeter {
 
+  /**
+  * This is a constructor
+  */
   
   public Greeter() {
 
   }
-
+  
+  /**
+  *@param spmeone name of a person
+  *@return greeting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello Elsa, %s!", someone);
   }
